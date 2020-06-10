f1931c468460bed04d6e1d5d589232c0484a675b
silvercondor <biocrypto@protonmail.com>
2020-06-10 12:22:14 +0800

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lazysql",
-    version="0.0.1",
+    version="0.0.2",
     author="silvercondor",
     author_email="silvercondor@protonmail.com",
     description="SQL connector for lazy people",
